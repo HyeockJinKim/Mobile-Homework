7693c926ee5f2bd6d7bb3928e0156cee5a8c838c
KimHyeockJin <gurwls9628@gmail.com>
2017-11-25 14:51:11 +0900

    1주차 과제

---
+++ MS_hw01_201502043/app/src/main/java/kr/ac/cnu/cse/ms_hw01_201502043/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.cnu.cse.ms_hw01_201502043;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
